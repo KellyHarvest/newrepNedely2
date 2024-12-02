cfa8dea3f04f3d6924c47babc129086526767178
Vlad <vladjjjsss6@gmail.com>
2024-12-02 18:57:17 +0500

    feat: category 1 day

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
+import {useFilmStore} from "~/stores/film";
+import {useCategoryStore} from "~/stores/category";
 
+const categoryStory = useCategoryStore();
+const filmStore = useFilmStore();
 </script>
 
 <template>
-  <div class="row">
+  <div class="row mt-5">
     <div class="col-md-4">
       <select class="form-select" aria-label="Default select example">
-        <option selected>Selecting a genre</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option :value="null" selected>Select country</option>
+        <option
+            v-for="category in categoryStory.categories"
+            :key="category.id"
+            :value="category.id">{{ category.name }} ({{category.filmCount}}) </option>
       </select>
     </div>
     <div class="col-md-4">
       <select class="form-select" aria-label="Default select example">
-        <option selected>Country selection</option>
+        <option selected>Select country</option>
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
@@ -29,35 +34,30 @@
     </div>
     <div class="col-md-2">   <button type="button" class="btn btn-outline-info">Reset</button></div>
   </div>
-  <div class="row row-cols-1 row-cols-md-3 g-4 mt-4">
-    <div class="col">
-      <div class="card">
-        <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/cf1970bc-3f08-4e0e-a095-2fb57c3aa7c6/600x900" class="card-img-top" alt="...">
+  <div v-if="!filmStore.isLoading" class="row row-cols-1 row-cols-md-3 g-4 mt-2">
+    <div class="col" v-for="film in filmStore.films" :key="film.id">
+      <div class="card h-100">
+        <img v-if="film.link_img" :src="film.link_img" class="card-img-top" alt="...">
+        <img v-else="film.link_img" src="https://img.icity.life/upload/no_poster.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Матрица</h5>
-          <p class="card-text">Мир Матрицы — это иллюзия, существующая только в бесконечном сне обреченного человечества. Холодный мир будущего, в котором люди — всего лишь батарейки в компьютерных системах.</p>
-          <p class="card-text">110 мин.</p>
+          <h5 class="card-title"> {{ film.name}}</h5>
+          <p class="card-text"> {{ film.raringAvg}}</p>
+          <p class="card-text"> {{ film.duration }} min.</p>
+          <p class="card-text">{{ film.category }} </p>
+          <template v-for="(category, index) in film.categories" :key="category.id">
+            {{ category.name + (index + 1 < film.categories.lenght ? "," : "") }}
+          </template>
         </div>
+        <button type="button" class="btn btn-success">Success</button>
       </div>
     </div>
-    <div class="col">
+  </div>
+  <div v-else class="d-flex justify-content-center mt-4">
+    <div class="spinner-grow" role="status">
+      <span class="visually-hidden">Loading...</span>
     </div>
   </div>
-  <div class="row row-cols-1 row-cols-md-3 g-4 mt-4">
-    <div class="col">
-      <div class="card">
-        <img src="https://upload.wikimedia.org/wikipedia/ru/4/4d/Avengers_Infinity_War_poster.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Мстители: Война бесконечности</h5>
-          <p class="card-text">Пока Мстители и их союзники продолжают защищать мир от различных опасностей, с которыми не смог бы справиться один супергерой, новая угроза возникает из космоса: Танос. Межгалактический тиран преследует цель собрать все шесть Камней Бесконечности — артефакты невероятной силы, с помощью которых можно менять реальность по своему желанию. Всё, с чем Мстители сталкивались ранее, вело к этому моменту — судьба Земли никогда ещё не была столь неопределённой.</p>
-          <p class="card-text">190 мин.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-    </div>
-  </div>
-  <nav aria-label="Page navigation example">
+  <nav class="mt-4 d-flex justify-content-center" aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item">
         <a class="page-link" href="#" aria-label="Previous">
